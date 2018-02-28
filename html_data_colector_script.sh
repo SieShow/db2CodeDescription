a1b4fa510a2661228c973c114cbcd628a449a879
Lucas Gomes <lucasgsm88@gmail.com>
2018-02-28 17:01:07 -0300

    Inserting content and script files

---
+++ html_data_colector_script.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+bash < (https://www.ibm.com/support/knowledgecenter/v1/content/en/SSEPEK_11.0.0/codes/src/tpc/n007.html > fulfile.html)
+
+arquive_val = 0
+value=$(<fulfile.html)
+
+bash < (https://www.ibm.com/support/knowledgecenter/v1/content/en/SSEPEK_11.0.0/codes/src/tpc/n007.html)
