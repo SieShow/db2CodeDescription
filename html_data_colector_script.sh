d44057d653c23b1dc2172d81cb9b11ce45138bf6
Lucas Gomes <lucasgsm88@gmail.com>
2018-03-01 16:51:30 -0300

    getted all informations about db2 codes

--- html_data_colector_script.sh
+++ html_data_colector_script.sh
@@ -5,4 +5,4 @@ bash < (https://www.ibm.com/support/knowledgecenter/v1/content/en/SSEPEK_11.0.0/
 arquive_val = 0
 value=$(<fulfile.html)
 
-bash < (https://www.ibm.com/support/knowledgecenter/v1/content/en/SSEPEK_11.0.0/codes/src/tpc/n007.html)
+#bash < (https://www.ibm.com/support/knowledgecenter/v1/content/en/SSEPEK_11.0.0/codes/src/tpc/n007.html)
