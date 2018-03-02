3bb52bf723de1fbcab711f5c6ab5b0a3d63891f5
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-03-01 21:22:24 -0300

    created base archteture for project to read txt file referent to sql codes

---
+++ CodesGenerationJSON/CodesGenerationJSON/FileReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CodesGenerationJSON
+{
+    class FileReader : IFileReader
+    {
+
+        object IFileReader.generateJsonFromFileRead(Db2CodeStyle style)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IFileReader.readFile(string filePath)
+        {
+            FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            BufferedStream buffer = new BufferedStream(stream);
+            StreamReader reader = new StreamReader(buffer);
+
+            string line;
+            while((line = reader.ReadLine()) != null)
+            {
+
+            }
+        }
+    }
+}
---
+++ CodesGenerationJSON/CodesGenerationJSON/KeyWord.cs
@@ -0,0 +1,15 @@
+﻿namespace CodesGenerationJSON
+{
+    /// <summary>
+    /// Contains the string referenced to each default code attribute
+    /// </summary>
+    public class KeyWord
+    {
+        public const string DESCRIPTION = "Description";
+        public const string EXPLANATION = "Explanation";
+        public const string PROGRAMER_RESPONSE = "Programmer Response";
+        public const string DESTINATION = "Destination";
+        public const string SYSTEM_ACTION = "System Action";
+        public const string NOTE = "Note";
+    }
+}
