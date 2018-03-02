788d65e8985ccd296f10510e1181c9ee87c9d6d6
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-03-01 21:25:08 -0300

    removed unnecessary files and added txt conversion of sql code pdf

--- html_data_colector_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-bash < (https://www.ibm.com/support/knowledgecenter/v1/content/en/SSEPEK_11.0.0/codes/src/tpc/n007.html > fulfile.html)
-
-arquive_val = 0
-value=$(<fulfile.html)
-
-#bash < (https://www.ibm.com/support/knowledgecenter/v1/content/en/SSEPEK_11.0.0/codes/src/tpc/n007.html)
